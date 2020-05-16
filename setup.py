07c77128d4278463152de1c2f7f6efdfda45e3a3
qangelot <q.angelot@gmail.com>
2020-05-16 11:49:44 +0200

    updating binomial and gaussian class

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions',
+        version='0.1',
+        description='Gaussian and binomial distributions',
+        packages=['distributions'],
+        zip_safe=False)
